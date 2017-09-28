4c363ea4ad386d7e9226803bb6f0c76ef06a62de
Tim Fisher <timothy.l.fisher@raytheon.com>
2017-09-28 11:18:23 -0500

    Issue #12501: update^Cnginemeta, ingest_engine, stream_engine recipes

--- anaconda_build/ingestEngine/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# set to folder containing the engine repo's
-cd $HOME/uframes/engines
-
-# extract the bzip'd TAR to the install location
-tar jxf ingest_engine.tar.bz2 -C $PREFIX
---
+++ anaconda_build/stream_engine/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# make and populate the installation folder with the code in the Git repo
+mkdir $PREFIX/stream_engine
+cd $SRC_DIR
+git submodule update --init --recursive
+cp -r * $PREFIX/stream_engine
+
+# set up links in the installation bin folder to key components
+cd $PREFIX/bin
+ln -s ../stream_engine/manage-streamng manage-streamng
