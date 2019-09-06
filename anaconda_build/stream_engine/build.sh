ec1937173ad417d8dfc885054497a67699953857
tlfish3638 <31706152+tlfish3638@users.noreply.github.com>
2019-09-06 12:01:44 -0500

    Ensure existence of $PREFIX/bin folder

--- anaconda_build/stream_engine/build.sh
+++ anaconda_build/stream_engine/build.sh
@@ -7,5 +7,8 @@ git submodule update --init --recursive
 cp -r * $PREFIX/stream_engine
 
 # set up links in the installation bin folder to key components
+if [[ ! -d $PREFIX/bin ]]; then
+    mkdir $PREFIX/bin
+fi
 cd $PREFIX/bin
 ln -s ../stream_engine/manage-streamng manage-streamng
